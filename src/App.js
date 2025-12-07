fef411fb9c5786c6084c5164a32f966b28ce38b9
Henry <henry.gabriel.alvarez@gmail.com>
2025-12-06 18:05:24 -0600

    Bienvenida

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido, a <code>Aseradero Monte Moria</code>, pagina en contruccion.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
