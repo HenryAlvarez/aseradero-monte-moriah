0d60a8dcbed08545982ad1d1b56dbb99c72789c8
Henry <henry.gabriel.alvarez@gmail.com>
2025-12-08 22:22:13 -0600

    fisrt deploay in domain

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img width={600} src={portada} alt='Aseradero Monte Moria' />
+        <img width={600} src={portada} alt='Aseradero Monte Moriah' />
         <p>
-          Bienvenidos, a <code>Aseradero Monte Moria</code>, pagina en contruccion.
+          Bienvenidos, a <code>Aseradero Monte Moriah</code>, pagina en contruccion.
         </p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
